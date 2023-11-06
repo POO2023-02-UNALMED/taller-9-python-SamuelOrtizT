218ea9ddde98baca0dbd8c499aa62e2a5e998eee
SamuelOrtizT <saortizt@unal.edu.co>
2023-11-06 12:39:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from tkinter import Tk, Button, Entry
 num_1 = ""
 num_2 = ""
 operador = ""
+resultado = ""
 
 def actualizar_pantalla(evento):
-    global num_1, num_2, operador
+    global num_1, num_2, operador, resultado
     boton = evento.widget
     valor = boton['text']
-    if (valor in "0123456789"):
+    if (valor in "0123456789."):
         if (operador == ""):
             num_1 += valor
         else:
@@ -27,23 +28,22 @@ def actualizar_pantalla(evento):
             elif operador=="*":
                    resultado += str(numero1*numero2)
             elif operador=="/":
-                if numero2 == 0:
-                    resultado = "No se puede dividir por 0"
-                else:
-                    resultado += str(numero1/numero2)
+                resultado += str(numero1/numero2)
             num_1=""
             num_2=""
             operador=""
 
 
-    pantalla.insert("end", num_1+operador+num_2)
+    pantalla.delete(0, "end")
+    pantalla.insert("end", num_1+operador+num_2+resultado)
+    resultado=""
 
 
 # Configuración ventana principal
 root = Tk()
 root.title("Calculadora POO")
 root.resizable(0,0)
-root.geometry("500x253")
+root.geometry("530x253")
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
@@ -51,35 +51,35 @@ pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=1)
 
 # Configuración botones
 boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_1.grid(row=1, column=0, padx=1, pady=1)
+boton_1.grid(row=1, column=0, padx=1, pady=1, sticky="ew")
 boton_2 = Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_2.grid(row=1, column=1, padx=1, pady=1)
+boton_2.grid(row=1, column=1, padx=1, pady=1, sticky="ew")
 boton_3 = Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_3.grid(row=1, column=2, padx=1, pady=1)
+boton_3.grid(row=1, column=2, padx=1, pady=1, sticky="ew")
 boton_4 = Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_4.grid(row=2, column=0, padx=1, pady=1)
+boton_4.grid(row=2, column=0, padx=1, pady=1, sticky="ew")
 boton_5 = Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_5.grid(row=2, column=1, padx=1, pady=1)
+boton_5.grid(row=2, column=1, padx=1, pady=1, sticky="ew")
 boton_6 = Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_6.grid(row=2, column=2, padx=1, pady=1)
+boton_6.grid(row=2, column=2, padx=1, pady=1, sticky="ew")
 boton_7 = Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_7.grid(row=3, column=0, padx=1, pady=1)
+boton_7.grid(row=3, column=0, padx=1, pady=1, sticky="ew")
 boton_8 = Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_8.grid(row=3, column=1, padx=1, pady=1)
+boton_8.grid(row=3, column=1, padx=1, pady=1, sticky="ew")
 boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2")
-boton_9.grid(row=3, column=2, padx=1, pady=1)
+boton_9.grid(row=3, column=2, padx=1, pady=1, sticky="ew")
 boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor="hand2")
-boton_igual.grid(row=4, column=0, columnspan=2, padx=1, pady=1)
+boton_igual.grid(row=4, column=0, columnspan=2, padx=1, pady=1, sticky="ew")
 boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0)
-boton_punto.grid(row=4, column=2, padx=1, pady=1)
+boton_punto.grid(row=4, column=2, padx=1, pady=1, sticky="ew")
 boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2")
-boton_mas.grid(row=1, column=3, padx=1, pady=1)
+boton_mas.grid(row=1, column=3, padx=1, pady=1, sticky="ew")
 boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2")
-boton_menos.grid(row=2, column=3, padx=1, pady=1)
+boton_menos.grid(row=2, column=3, padx=1, pady=1, sticky="ew")
 boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2")
-boton_multiplicacion.grid(row=3, column=3, padx=1, pady=1)
+boton_multiplicacion.grid(row=3, column=3, padx=1, pady=1, sticky="ew")
 boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2")
-boton_division.grid(row=4, column=3, padx=1, pady=1)
+boton_division.grid(row=4, column=3, padx=1, pady=1, sticky="ew")
 
 
 boton_1.bind("<Button-1>", actualizar_pantalla)
